8f1a218632e8e1575a590ecd6e732b8429613a59
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-01 16:35:51 +0200

    Finished 'JS Fundamentals - Part 3' Assignment.

--- JavaScript foundations/Fundamentals3Assignment.js
+++ JavaScript foundations/Fundamentals3Assignment.js
@@ -1,24 +1,24 @@
 // Write a function called add7 that takes one number and returns that number + 7.
 function add7 (number)
 {
-
+    return (number + 7);
 }
 
 // Write a function called multiply that takes 2 numbers and returns their product.
 function multiply (a, b)
 {
-
+    return (a * b);
 }
 
 // Write a function called capitalize that takes a string and returns that string with only the first letter capitalized.
 // Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
 function capitalize(string)
 {
-
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
 // Write a function called lastLetter that takes a string and returns the very last letter of that string:
 function lastLetter(string)
 {
-
+    return string.charAt(string.length - 1);
 }
