704091860d1317d95c70818daa2467c364f42aa8
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-05 17:17:54 +0200

    Finished 'Reverse String' Exercise.

--- javascript-exercises/03-reverseString/reverseString.js
+++ javascript-exercises/03-reverseString/reverseString.js
@@ -1,5 +1,29 @@
-const reverseString = function () {
+const reverseString = function(sentence)
+{
+    return sentence.split("").reverse().join("");
 
+    /*
+    By first princples...
+
+    // Splitting of the original into letters.
+    let arr = sentence.split("");
+
+    // Actual reversal of the original - still split into letters.
+    let reversedArr = [];
+    for (let i = arr.length-1; i >= 0; i--)
+    {
+        reversedArr.push(arr[i]);
+    }
+    
+    // Recombining of the reversed list into string.
+    let reversedString = "";
+    for (let i = 0; i < arr.length; i++)
+    {
+        reversedString += reversedArr[i];
+    }
+
+    return reversedString;
+    */
 };
 
 // Do not edit below this line
