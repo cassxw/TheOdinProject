18191c377aaafbfdb0a89fbebf810512ecc7dab5
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-05 17:36:58 +0200

    Finished 'Remove From Array' Exercise.

--- javascript-exercises/04-removeFromArray/removeFromArray.js
+++ javascript-exercises/04-removeFromArray/removeFromArray.js
@@ -1,8 +1,37 @@
-const removeFromArray = function()
+const removeFromArray = function(arr, ...args)
 {
-        
-    
+    let newArr = [];
+    arr.forEach( (item) => {
 
+        if (!args.includes(item)) //If the current item is not to be removed, then add it.
+        {
+            newArr.push(item);
+        }
+
+    });
+    return newArr;
+
+    /*
+    Using the filter method...
+
+    return arr.filter(item => !args.includes(item));
+    */
+
+    /* 
+    First principles...
+
+    for (let i = 0; i < args.length; i++)
+    {
+        for (let k = 0; k < arr.length; k++)
+        {
+            if (arr[k] === args[i]) //Strict equality checker (same type).
+            {
+                arr.splice(k, 1);
+            }
+        }
+    }
+    return arr;
+    */
 };
 
 // Do not edit below this line
