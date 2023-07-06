78badb57915f363411045a2ffe233c6f2c7c2f7b
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-06 07:37:23 +0200

    Finished 'Sum All' Exercise.

--- javascript-exercises/05-sumAll/sumAll.js
+++ javascript-exercises/05-sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function(num1, num2)
+const sumAll = function(min, max)
 {
+    // Number type-check && Positive
+    if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0)
+    {
+        return "ERROR";
+    }
 
+    let sum = 0;
+
+    if (min > max) //if the max is actually the min.
+    {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    for (let i = min; i <= max; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
