85bcb36973d002a13e27aed1f011d82468368d7f
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-06 08:52:04 +0200

    Finished 'Temperature Conversion' Exercise.

--- javascript-exercises/07-tempConversion/tempConversion.js
+++ javascript-exercises/07-tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const convertToCelsius = function()
+const convertToCelsius = function(fahrenheitVal)
 {
-
+    return Math.round(((fahrenheitVal - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function()
+const convertToFahrenheit = function(celsiusVal)
 {
-
+    return Math.round((celsiusVal * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
